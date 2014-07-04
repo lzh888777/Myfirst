c74f1fdfd55ba1f7aa8648075694bff5bb817022
刘志华 <liuzhihua@liuzhihuadeMacBook-Pro.local>
2014-07-04 12:32:23 +0800

    Initial Commit

---
+++ Myfirst/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Myfirst
+//
+//  Created by 刘志华 on 14-7-4.
+//  Copyright (c) 2014年 刘志华. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
